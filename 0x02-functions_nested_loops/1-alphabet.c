16e8bb27d580b434c31aaa3505aace994cfe28af
geraldokoye <gerald3641@gmail.com>
2022-10-20 05:23:20 +0000

    ok

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 include "main.h"
+
 /**
  * print_alphabet - Prints lowercase of alphabets
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
-
 void print_alphabet(void)
 {
 	char alpha;
