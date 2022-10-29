2fcadaed3d38ac88707028802088243fa8740e6d
geraldokoye <gerald3641@gmail.com>
2022-10-29 06:38:18 +0000

    ok

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stout
- * @c: The char to be printed
+ * _putchar - writes the character c to stdout
+ * @c: char to be printed
  * Return: On success 1
  * */
 int _putchar(char c)
