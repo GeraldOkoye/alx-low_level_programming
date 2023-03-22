f5077b5b9a61a0fc0a288b9e8e9bb389238811d3
geraldokoye <gerald3641@gmail.com>
2023-03-22 02:59:12 +0000

    ok

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print name of file therein program is complied
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+
+	return (0);
+}
